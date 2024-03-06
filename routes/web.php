9cb290a94585d1d56539c2ae42cbe2b40511b146
ZhadanD <zhdv07@mail.ru>
2024-03-06 19:56:25 +0300

    Реализовал разграничение по двум ролям: пользователя и админа.

--- routes/web.php
+++ routes/web.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// ['auth', 'verified']
-
 Route::group(['namespace' => 'App\Http\Controllers'], function () {
    Route::get('/', IndexController::class)->name('main');
+
+   Route::group(['namespace' => 'Admin', 'prefix' => 'admin', 'middleware' => ['auth', 'verified', 'admin']], function () {
+        Route::get('/', 'IndexController')->name('admin.main');
+   });
 });
 
 Route::middleware('auth')->group(function () {
