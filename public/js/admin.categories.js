f9261d25170c41cbac19a421306d7d69c8c4f068
ZhadanD <zhdv07@mail.ru>
2024-03-21 18:18:11 +0300

    Поправил сообщение при удалении категории.

--- public/js/admin.categories.js
+++ public/js/admin.categories.js
@@ -1,7 +1,7 @@
 function deleteCategory(categoryName, categoryId) {
     event.preventDefault()
 
-    let decision = confirm('Вы точно хотите удалить категорию \"' + categoryName + '\"?')
+    let decision = confirm(`Вы точно хотите удалить категорию "${categoryName}"?\nВсе товары, принадлежащие этой категории, будут откреплены.`)
 
     if (decision) document.getElementById(`delete-category-${categoryId}`).submit()
 }
